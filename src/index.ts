b2cdad1b032efc5e59273a5d28de8b90f31b0c76
Naing Lin Aung <nainglinaung@s2t.ai>
2023-02-12 14:34:03 +0630

    init commit

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
-// eslint-disable-next-line no-console
-console.log('Hello world!');
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+// console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
